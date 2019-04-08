b5658f1499c90ab9a7ab472e0adc81c2b183426d
Camilo Sanchez <camilin87@gmail.com>
2019-04-08 17:09:41 -0400

    include the sample python code in this repo

--- src/main/java/com/tddapps/learnrpi/Program.java
+++ src/main/java/com/tddapps/learnrpi/Program.java
@@ -2,23 +2,6 @@ package com.tddapps.learnrpi;
 
 import com.pi4j.io.gpio.*;
 
-/*
-# python equivalent code
-
-import RPi.GPIO as GPIO
-import time
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.OUT)
-
-GPIO.output(18, True)
-time.sleep(2)
-GPIO.output(18, False)
-
-GPIO.cleanup()
-
-*/
-
 public class Program {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Starting");
