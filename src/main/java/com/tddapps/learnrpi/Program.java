9aef15a69105765c95293e9f98e6cfb632cddc48
Camilo Sanchez <camilin87@gmail.com>
2019-04-22 08:18:21 -0400

    use the extracted stepper motor abstraction

--- src/main/java/com/tddapps/learnrpi/Program.java
+++ src/main/java/com/tddapps/learnrpi/Program.java
@@ -2,10 +2,11 @@ package com.tddapps.learnrpi;
 
 import com.pi4j.io.gpio.*;
 import lombok.Data;
+import lombok.extern.log4j.Log4j2;
 
-import java.util.Arrays;
 import java.util.concurrent.Executors;
 
+@Log4j2
 public class Program {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Starting");
@@ -37,40 +38,32 @@ public class Program {
 
         Executors.newSingleThreadExecutor().execute(() -> {
             // These are BCM GPIO pins 17, 18, 27 and 22. Numbers are different because of wiringpi
-            try {
-                var config = new MotorMoveConfig();
-                config.setName("X");
-                config.setPinIds(new Pin[]{
-                        RaspiPin.GPIO_00,
-                        RaspiPin.GPIO_01,
-                        RaspiPin.GPIO_02,
-                        RaspiPin.GPIO_03
-                });
-                config.setClockwise(true);
-                config.setRevertMovementAfterSteps(-1);
-                MoveSingleStepper(gpio, config);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            var config = new MotorMoveConfig();
+            config.setName("X");
+            config.setPinIds(new Pin[]{
+                    RaspiPin.GPIO_00,
+                    RaspiPin.GPIO_01,
+                    RaspiPin.GPIO_02,
+                    RaspiPin.GPIO_03
+            });
+            config.setClockwise(true);
+            config.setRevertMovementAfterSteps(-1);
+            MoveSingleStepper(gpio, config);
         });
 
         Executors.newSingleThreadExecutor().execute(() -> {
             // These are BCM GPIO pins 23, 24, 25 and 04. Numbers are different because of wiringpi
-            try {
-                var config = new MotorMoveConfig();
-                config.setName("Y");
-                config.setPinIds(new Pin[]{
-                        RaspiPin.GPIO_04,
-                        RaspiPin.GPIO_05,
-                        RaspiPin.GPIO_06,
-                        RaspiPin.GPIO_07
-                });
-                config.setClockwise(false);
-                config.setRevertMovementAfterSteps(10000);
-                MoveSingleStepper(gpio, config);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            var config = new MotorMoveConfig();
+            config.setName("Y");
+            config.setPinIds(new Pin[]{
+                    RaspiPin.GPIO_04,
+                    RaspiPin.GPIO_05,
+                    RaspiPin.GPIO_06,
+                    RaspiPin.GPIO_07
+            });
+            config.setClockwise(false);
+            config.setRevertMovementAfterSteps(7500);
+            MoveSingleStepper(gpio, config);
         });
     }
 
@@ -80,65 +73,35 @@ public class Program {
         private Pin[] pinIds;
         private boolean clockwise;
         private int revertMovementAfterSteps;
+
+        public void ReverseDirection() {
+            clockwise = !clockwise;
+        }
     }
 
-    private static void MoveSingleStepper(GpioController gpio, MotorMoveConfig config) throws InterruptedException {
-        var stepPins = Arrays.stream(config.getPinIds())
-                .map(p -> {
-                    var pin = gpio.provisionDigitalOutputPin(p, p.toString(), PinState.LOW);
-                    pin.setShutdownOptions(true, PinState.LOW);
-                    pin.low();
-                    return pin;
-                })
-                .toArray(GpioPinDigitalOutput[]::new);
+    private static void MoveSingleStepper(GpioController gpio, MotorMoveConfig config) {
+        var motor = new StepperMotorUln(config.getName(), gpio, config.getPinIds());
+        motor.Init();
 
-        final var movementSequence = new int[][]{
-                new int []{1, 0, 0, 1},
-                new int []{1, 0, 0, 0},
-                new int []{1, 1, 0, 0},
-                new int []{0, 1, 0, 0},
-                new int []{0, 1, 1, 0},
-                new int []{0, 0, 1, 0},
-                new int []{0, 0, 1, 1},
-                new int []{0, 0, 0, 1}
-        };
-
-        var pinCount = stepPins.length;
-        var stepCount = movementSequence.length;
-        var stepIndex = 0;
         var stepNumber = 0L;
-        var stepDirection = config.isClockwise() ? 1 : -1;
 
-        while(true){
-            var currentStep = movementSequence[stepIndex];
-
-            System.out.println(String.format("[%s] Step; index: %d; pins: %s;",
-                    config.getName(), stepIndex, Arrays.toString(currentStep)));
-
-            for (int i = 0; i < pinCount; i++) {
-                if (currentStep[i] == 0){
-                    stepPins[i].low();
+        try {
+            while (true) {
+                if (config.isClockwise()) {
+                    motor.MoveCW();
+                } else {
+                    motor.MoveCCW();
                 }
-                else {
-                    System.out.println(String.format("[%s] Enable %s", config.getName(), stepPins[i].getName()));
-                    stepPins[i].high();
+
+                stepNumber++;
+                if (config.getRevertMovementAfterSteps() > 0 && (stepNumber % config.getRevertMovementAfterSteps() == 0)) {
+                    config.ReverseDirection();
                 }
             }
-
-            stepIndex += stepDirection;
-            if (stepIndex >= stepCount){
-                stepIndex = 0;
-            }
-            if (stepIndex < 0){
-                stepIndex = stepCount - 1;
-            }
-
-            stepNumber++;
-            if (config.getRevertMovementAfterSteps() > 0 && (stepNumber % config.getRevertMovementAfterSteps() == 0)){
-                stepDirection *= -1;
-            }
-
-            Thread.sleep(1);
+        } catch (InvalidOperationException e) {
+            log.error("Invalid Motor Operation", e);
+        } finally {
+            motor.Destroy();
         }
     }
 }
